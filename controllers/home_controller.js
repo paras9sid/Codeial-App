695d1b69304dbe58e3174f7446fc240aca9135cc
paras9sid <sidharthjainp9@gmail.com>
2022-08-17 17:54:29 +0530

    Passport Authentication enabled

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,20 +1,53 @@
+// const Post = require('../models/post');
+
+// module.exports.home = function(req,res){
+//     // return res.end('<h1>Express is up for codeial</h1>');
+//     // console.log(req.cookies);// will print cookies written in inpect--application -storage --cokies
+//     // res.cookie('',18); // explicity cookie value changed -- will reflect in inspect-applciation-cookie
+//     // return res.render('home',{
+//     //     title:"Home"
+//     // });
+
+//     // posts/comment in db
+   
+//     // Post.find({},function(err,posts){
+//     //     return res.render('home',{
+//     //         title:"Codeial | Home",
+//     //         posts
+//     //     });
+//     // });
+
+//      //populating db (populate the user of each post) - google search -- mongoose populate
+//      Post.find({}).populate('user').exec(function(err,posts){
+//         return res.render('home',{
+//             title:"Codeial | Home",
+//             posts
+//         });
+//      });
+
+// };
+
 const Post = require('../models/post');
 
-module.exports.home = function(req,res){
-    // return res.end('<h1>Express is up for codeial</h1>');
-    // console.log(req.cookies);// will print cookies written in inpect--application -storage --cokies
-    // res.cookie('',18); // explicity cookie value changed -- will reflect in inspect-applciation-cookie
-    // return res.render('home',{
-    //     title:"Home"
+module.exports.home = function(req, res){
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts:  posts
+    //     });
     // });
 
-    // posts/comment in db
-
-    Post.find({},function(err,posts){
-        return res.render('home',{
-            title:"Codeial | Home",
-            posts
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
         });
-    });
+    })
 
-};
+}
+
+// module.exports.actionName = function(req, res){}
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 
-router.post('/create',postsController.create);
+//second level of Authentication , so nobody can tamper with inspect element and spam the page , only signed user can post 
+// checkAuthentication method is used
+router.post('/create',passport.checkAuthentication,postsController.create);
 
 module.exports = router;
