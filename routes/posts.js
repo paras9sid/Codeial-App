f125afe7e16c4c001dd503e352c01a4f2eb3302a
paras9sid <sidharthjainp9@gmail.com>
2022-11-30 19:32:16 +0530

    Check Authentication implemented so no one can fiddle with site w/o sign in

--- routes/posts.js
+++ routes/posts.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const postsController = require('../controllers/posts_controller');
 
-//second level of Authentication , so nobody can tamper with inspect element and spam the page , only signed user can post 
-// checkAuthentication method is used
-// router.post('/create',passport.checkAuthentication,postsController.create);
-router.post('/create',postsController.create);  // synced with post model
+// second level of Authentication , so nobody can tamper with inspect element and spam the page , only signed user can post 
+// checkAuthentication method is used - user cant edit html from inpect tool and fiddle with our website , authentication needed now
+router.post('/create',passport.checkAuthentication,postsController.create);
+
+router.post('/create',postsController.create);  // synced with postController 
+
 // router.get('/destroy/:id',passport.checkAuthentication , postsController.destroy);
 
 
