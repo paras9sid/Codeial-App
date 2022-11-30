9737589dedbe48a4b04fc36877848f92fe26c4d0
paras9sid <sidharthjainp9@gmail.com>
2022-11-30 18:49:04 +0530

    Posts saving to db when click post button

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,33 +4,33 @@ const Comment = require('../models/comment');
 
 module.exports.create = function(req,res){
     Post.create({
-        content: req.body.content,
-        user : req.user._id
+        content: req.body.content, // content = name of textarea name(home.ejs file) like input tag box --
+        user : req.user._id   // user = logged in user
     }, function(err,post){
             if(err){
-                console.log('error in creating a post');
+                console.log('Error in creating a post!!');
                 return;
             }
             return res.redirect('back');
         });
 }
 
-//action for deleting post
+// //action for deleting post
 
-module.exports.destroy = function(req ,res){
+// module.exports.destroy = function(req ,res){
     
-    //finding posts in db
-    Post.findById(req.params.id , function(err,post){
-        //post found
-        //.id = conveting the object id into string -> _id converted to .id by mongoose implicitily
-        if(post.user == request.user.id){  //
-            post.remove();
+//     //finding posts in db
+//     Post.findById(req.params.id , function(err,post){
+//         //post found
+//         //.id = conveting the object id into string -> _id converted to .id by mongoose implicitily
+//         if(post.user == request.user.id){  //
+//             post.remove();
 
-            Comment.deleteMany({post:req.params.id},function(err){
-                return res.redirect('back');
-            });
-        }else{
-            return res.redirect('back');
-        }
-    });
-}
+//             Comment.deleteMany({post:req.params.id},function(err){
+//                 return res.redirect('back');
+//             });
+//         }else{
+//             return res.redirect('back');
+//         }
+//     });
+// }
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
-// const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 
@@ -8,6 +8,7 @@ const postsController = require('../controllers/posts_controller');
 // checkAuthentication method is used
 // router.post('/create',passport.checkAuthentication,postsController.create);
 router.post('/create',postsController.create);
+// router.get('/destroy/:id',passport.checkAuthentication , postsController.destroy);
 
 
 module.exports = router;
