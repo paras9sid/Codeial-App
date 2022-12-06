5ba6fd9fe7d0a0b4a94663f052f2f55453bcdcde
paras9sid <sidharthjainp9@gmail.com>
2022-12-06 14:43:24 +0530

    JWT token creation done

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,29 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+
+//get login data session creation - by using jsonwebtoken - to run ocde below we need a route
+module.exports.createSession = async function(req,res){
+    try{
+        let user = await User.findOne({email: req.body.email});
+
+        if(!User || user.password != req.body.password){
+            return res.status(422).json({
+                message:"Invalid Username or password!"
+            });
+        }
+
+        return res.status(200).json({
+            message:"Sign in succcessful!",
+            data: {
+                token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '10000'})
+            }
+        });
+        
+    }catch(err){
+        console.log('****err',err);
+        return res.status(500).json({
+            message:"Internal Server Error"
+        });
+    }
+}
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -5,4 +5,7 @@ const router = express.Router();
 //post router
 router.use('/posts',require('./posts'));
 
+router.use('/users',require('./users'));
+
+
 module.exports = router;
