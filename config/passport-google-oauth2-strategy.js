125b8f9b9b5a818396b5648d93206af79fdd691d
paras9sid <sidharthjainp9@gmail.com>
2022-12-22 17:24:50 +0530

    Google OAuth toString prob fixed

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -34,7 +34,7 @@ passport.use(new googleStrategy({
                 User.create({
                     name:profile.displayName,
                     email:profile.emails[0].value,
-                    password: crypto.randomBytes[20].toString('hex')
+                    password:crypto.randomBytes(20).toString('hex')
 
                 }, function(err,user){
                     if(err){
