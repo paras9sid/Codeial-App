79aa451fa0c314391f79b0fe6707e99eb9fb75bc
paras9sid <sidharthjainp9@gmail.com>
2023-02-03 16:07:29 +0530

    comment_controller updated-1 in between

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,47 +1,6 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-// module.exports.create = function(req,res){ 
-//     Post.findById(req.body.post,function(err,post){ // finding post first with post id
-//         if(post){   // if post is found
-//             Comment.create({   // action create -- created for comments
-//                 content: req.body.content,
-//                 post:req.body.post,
-//                 user: req.user._id
-//             },function(err,comment){
-//                 //handle error - post not found
-//                 if(err){console.log(err);}
-//                 post.comments.push(comment);  //comment pushed to the post
-//                 post.save();  // save tells the db thats it final version - save it
-
-//                 res.redirect('/');
-//             });
-//         }
-//     });
-// }
-
-// module.exports.destory = function(req,res){
-    
-//     Comment.findById(req.params.id,function(err,comment){
-        
-//         if(comment.user == req.user.id){  // like done for post
-
-//             //fetch post id of the particular comment first
-
-//             let postId = comment.post;
-
-//             comment.remove();
-
-//             Post.findByIdAndUpdate(postId,{ $pull: { comments:req.params.id }},function(err,post){
-//                 return res.redirect('back');
-//             })
-//         }else{
-//             return res.redirect('back');
-//         }
-//     });
-// }
-
-
 //converting to async await
 
 module.exports.create = async function(req,res){ 
@@ -58,7 +17,19 @@ module.exports.create = async function(req,res){
                 
                 post.comments.push(comment);  //comment pushed to the post
                 post.save();  // save tells the db thats it final version - save it
-    
+                
+
+                if(req.xhr){
+                    return res.status(200).json({
+                        data:{
+                            comment,
+                        },
+                        message:"Post created"
+                    })
+
+                }
+
+                req.flash('success','Comment published!');
                 res.redirect('/');
             }
         
@@ -84,10 +55,23 @@ module.exports.destory = async function(req,res){
                          comments:req.params.id 
                         }
                     });
+
+                    //send the comment id which was deleted back to the views
+                    if(req.xhr){
+                        return res.status(200).json({
+                            data:{
+                                comment_id:req.params.id
+                            },
+                            message:"Post deleted"
+                        });
+                    }
+
+                    req.flash('success','Comment deleted');
                 
                 return res.redirect('back');
                 
             }else{
+                req.flash('error','Unauthorized');
                 return res.redirect('back');
             }
         
---
+++ mailers/comments_mailer.js
@@ -0,0 +1,27 @@
+// comments related mails
+
+import nodemailer from '../config/nodemailer';
+
+
+//this is another way of exporting a method - using arow functions
+exports.newComment = (comment) => {
+    console.log('inside newComment mailer');
+
+    nodemailer.transporter.sendMail({
+        from : '@gmail.com',
+        to : comment.user.email,
+        subject : 'new comment published',
+        body:'<h1>you,your comment is published</h1>',
+
+    },
+    //callback function 
+    (err,info) => {
+        if(err){
+            console.log('error sending mail ' , err);
+            return;
+        }
+
+        console.log('Message sent',info);
+        return;
+    });
+}
