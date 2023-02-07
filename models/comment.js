85e064e9719298c54e7faa187949780f65f331de
paras9sid <sidharthjainp9@gmail.com>
2023-02-07 16:48:48 +0530

    Schema for likes created

--- models/comment.js
+++ models/comment.js
@@ -13,7 +13,14 @@ const commentSchema = new mongoose.Schema({
     post:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Post'
-    }},
+    },
+    likes: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Like'
+        }
+    ]   
+},
     {
         timestamps:true
     
